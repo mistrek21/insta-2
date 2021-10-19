698eee2eae6384f591b5a70e49c9c40975e74fe4
mata <mata@matas-MacBook-Air.local>
2021-10-20 01:14:37 +0200

    add modal and first look to the button with styling to add pictures

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 
 export default function Home() {
   return (
@@ -12,9 +13,13 @@ export default function Home() {
 
       {/* Header */}
       <Header />
+
       {/* Feed */}
       <Feed />
+
       {/* Modal */}
+      <Modal />
+
     </div>
   )
 }
