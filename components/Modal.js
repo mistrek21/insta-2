b08176e8de3ff1e47ec172e1fdd482712738b93e
mata <mata@matas-MacBook-Air.local>
2021-10-20 14:53:13 +0200

    upload post to firestore upload image to direbase storage get url and update the new doc with the image WE CREATED UPLOADPOST TO FIREBASE

--- components/Modal.js
+++ components/Modal.js
@@ -3,6 +3,10 @@ import { modalState } from "../atoms/modalAtom"
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useRef, useState } from "react"
 import { CameraIcon } from "@heroicons/react/solid"
+import { db, storage } from '../firebase'
+import { addDoc, serverTimestamp, collection, updateDoc, doc } from "firebase/firestore"
+import { useSession } from "next-auth/react"
+import { ref, uploadString, getDownloadURL } from "firebase/storage"
 
 function Modal() {
     const [open, setOpen] = useRecoilState(modalState)
@@ -10,6 +14,7 @@ function Modal() {
     const [selectedFile, setSelectedFile] = useState(null)
     const captionRef = useRef(null)
     const [loading, setLoading] = useState(false)
+    const { data: session } = useSession()
 
     const addImageToPost = (e) => {
         const reader = new FileReader()
@@ -26,6 +31,34 @@ function Modal() {
         if (loading) return
 
         setLoading(true)
+
+        // Create a post and add to firestore "posts" collection
+        // Get the post id for the newly crated post
+        // upload the image to firebase storage with the post id 
+        // get a download url from firebase storage and update the original post with image
+
+        const docRef = await addDoc(collection(db, 'posts'), {
+            username: session.user.username,
+            caption: captionRef.current.value,
+            profileImg: session.user.image,
+            timeStamp: serverTimestamp()
+        })
+
+        console.log("New doc added with ID", docRef.id)
+
+        const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+        await uploadString(imageRef, selectedFile, "data_url").then(async snapshot => {
+            const downloadURL = await getDownloadURL(imageRef)
+
+            await updateDoc(doc(db, 'posts', docRef.id), {
+                image: downloadURL
+            })
+        })
+
+        setOpen(false)
+        setLoading(false)
+        setSelectedFile(null)
     }
 
     return (
@@ -106,9 +139,11 @@ function Modal() {
                                 <div className="mt-5 sm:mt-6">
                                     <button
                                         type="button"
+                                        disabled={!selectedFile}
                                         className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-300"
+                                        onClick={uploadPost}
                                     >
-                                        Upload Post
+                                        {loading ? "Uploading..." : "Upload Post"}
                                     </button>
 
                                 </div>
