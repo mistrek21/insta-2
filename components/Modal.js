8e17682665a7825898a5cc003d34c31576ee65f8
mata <mata@matas-MacBook-Air.local>
2021-10-20 14:23:10 +0200

    modal create a way to upload pics with refs and usestate

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,32 @@
 import { useRecoilState } from "recoil"
 import { modalState } from "../atoms/modalAtom"
 import { Dialog, Transition } from '@headlessui/react'
-import { Fragment } from "react"
+import { Fragment, useRef, useState } from "react"
+import { CameraIcon } from "@heroicons/react/solid"
 
 function Modal() {
     const [open, setOpen] = useRecoilState(modalState)
+    const filePickRef = useRef(null)
+    const [selectedFile, setSelectedFile] = useState(null)
+    const captionRef = useRef(null)
+    const [loading, setLoading] = useState(false)
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader()
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result)
+        }
+    }
+
+    const uploadPost = async () => {
+        if (loading) return
+
+        setLoading(true)
+    }
 
     return (
         <Transition.Root show={open} as={Fragment}>
@@ -38,9 +60,50 @@ function Modal() {
                     >
                         <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                             <div>
-                                <div className="mt-5 sm:mt-6">
 
-                                    
+                                {selectedFile ? (
+                                    <img src={selectedFile} onClick={() => setSelectedFile(null)} alt="" className="w-full object-contain cursor-pointer" />
+                                ) : (
+                                    <div
+                                        onClick={() => filePickRef.current.click()}
+                                        className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                                    >
+                                        <CameraIcon
+                                            className="h-6 w-6 text-red-600"
+                                            area-hidden="true"
+                                        />
+                                    </div>
+                                )}
+                                <div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title
+                                            as="h3"
+                                            className="text-lg leading-6 font-medium text-gray-900"
+                                        >
+                                            Upload a photo
+                                        </Dialog.Title>
+
+                                        <div>
+                                            <input
+                                                ref={filePickRef}
+                                                type="file"
+                                                hidden
+                                                onChange={addImageToPost}
+                                            />
+                                        </div>
+
+                                        <div>
+                                            <input
+                                                className="border-none focus:ring-0 w-full text-center"
+                                                type="text"
+                                                ref={captionRef}
+                                                placeholder="Please enter a caption..."
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="mt-5 sm:mt-6">
                                     <button
                                         type="button"
                                         className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-300"
@@ -54,7 +117,7 @@ function Modal() {
                     </Transition.Child>
                 </div>
             </Dialog>
-        </Transition.Root>
+        </Transition.Root >
     )
 }
 
